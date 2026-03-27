79bfaa22033dc1ca6a384f8018f393e3ba460ec8
Ngoc Trong <trongpvn2211@gmail.com>
2026-03-27 09:52:21 +0700

    add field for role entity

--- src/main/java/com/example/sneaker_store/model/PermissionEntity.java
+++ src/main/java/com/example/sneaker_store/model/PermissionEntity.java
@@ -2,11 +2,13 @@ package com.example.sneaker_store.model;
 
 import com.example.sneaker_store.service.impl.AuthServiceImpl;
 import com.example.sneaker_store.util.enumEntity.MethodPermission;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
+import java.util.List;
 
 @Getter
 @Setter
@@ -29,6 +31,10 @@ public class PermissionEntity {
     private String createdBy;
     private Instant updatedAt;
     private String updatedBy;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "permissions")
+    @JsonIgnore
+    private List<RoleEntity> roles;
 
     @PrePersist
     public void create(){
--- src/main/java/com/example/sneaker_store/model/RoleEntity.java
+++ src/main/java/com/example/sneaker_store/model/RoleEntity.java
@@ -1,13 +1,49 @@
 package com.example.sneaker_store.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.sneaker_store.service.impl.AuthServiceImpl;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Instant;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "tbl_role")
 public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean active;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "tbl_role_permission",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    @JsonIgnoreProperties(value = {"roles"})
+    private List<PermissionEntity> permissions;
+
+    @PrePersist
+    public void create(){
+        this.createdAt = Instant.now();
+        this.createdBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
+
+    @PreUpdate
+    public void update(){
+        this.updatedAt = Instant.now();
+        this.updatedBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
 }
