8373c8e064542f0cfc6985eb7962798ed456fed3
Ngoc Trong <trongpvn2211@gmail.com>
2026-03-30 11:20:34 +0700

    add field in class category entity and base file create response, request for category

--- src/main/java/com/example/sneaker_store/model/CategoryEntity.java
+++ src/main/java/com/example/sneaker_store/model/CategoryEntity.java
@@ -1,13 +1,45 @@
 package com.example.sneaker_store.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.sneaker_store.service.impl.AuthServiceImpl;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "tbl_category")
 @Getter
 @Setter
 public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    private Long parentId;
+
+    private Instant createdAt;
+    private String createdBy;
+    private Instant updatedAt;
+    private String updatedBy;
+
+    @PrePersist
+    public void create(){
+        this.createdAt = Instant.now();
+        this.createdBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
+
+    @PreUpdate
+    public void update(){
+        this.updatedAt = Instant.now();
+        this.updatedBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
 }
