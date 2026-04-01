3f733656c7d62f73bd6f9942482169a1d47e2942
Ngoc Trong <trongpvn2211@gmail.com>
2026-04-01 21:29:28 +0700

    add full field for product image entity

--- src/main/java/com/example/sneaker_store/model/ProductImageEntity.java
+++ src/main/java/com/example/sneaker_store/model/ProductImageEntity.java
@@ -1,13 +1,41 @@
 package com.example.sneaker_store.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.sneaker_store.service.impl.AuthServiceImpl;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.Instant;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "tbl_product_image")
 public class ProductImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageURL;
+
+    private boolean isMain;
+
+    private Instant createdAt;
+    private String createdBy;
+    private Instant updatedAt;
+    private String updatedBy;
+
+    @PrePersist
+    public void create(){
+        this.createdAt = Instant.now();
+        this.createdBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
+
+    @PreUpdate
+    public void update(){
+        this.updatedAt = Instant.now();
+        this.updatedBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
 }
