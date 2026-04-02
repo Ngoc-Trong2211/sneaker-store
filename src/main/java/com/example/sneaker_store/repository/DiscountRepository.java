c840e0cd0510795b2e079c6a7550ddec14728721
Ngoc Trong <trongpvn2211@gmail.com>
2026-04-02 08:46:30 +0700

    add base handle discount entity

---
+++ src/main/java/com/example/sneaker_store/repository/DiscountRepository.java
@@ -0,0 +1,7 @@
+package com.example.sneaker_store.repository;
+
+import com.example.sneaker_store.model.DiscountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiscountRepository extends JpaRepository<DiscountEntity, Long> {
+}
---
+++ src/main/java/com/example/sneaker_store/service/impl/DiscountServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.sneaker_store.service.impl;
+
+import com.example.sneaker_store.service.DiscountService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j(topic = "DISCOUNT-SERVICE")
+@RequiredArgsConstructor
+public class DiscountServiceImpl implements DiscountService {
+}
