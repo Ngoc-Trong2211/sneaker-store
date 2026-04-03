24203f2ba714d6e66de9e4868df5495a9a7a83de
Ngoc Trong <trongpvn2211@gmail.com>
2026-04-03 10:43:26 +0700

    add base handle product variant entity

---
+++ src/main/java/com/example/sneaker_store/controller/ProductVariantController.java
@@ -0,0 +1,15 @@
+package com.example.sneaker_store.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j(topic = "PRODUCT-VARIANT-CONTROLLER")
+@RequestMapping("/product-variant/v1")
+public class ProductVariantController {
+    
+}
---
+++ src/main/java/com/example/sneaker_store/service/impl/ProductVariantServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.sneaker_store.service.impl;
+
+import com.example.sneaker_store.service.ProductVariantService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j(topic = "PRODUCT-VARIANT-SERVICE")
+@RequiredArgsConstructor
+public class ProductVariantServiceImpl implements ProductVariantService {
+    
+}
