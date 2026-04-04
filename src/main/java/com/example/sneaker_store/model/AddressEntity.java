d1b899ac3e30f6b2f8ee38eab46bcef357ee5821
Ngoc Trong <trongpvn2211@gmail.com>
2026-04-04 17:00:12 +0700

    add full field for address entity

--- src/main/java/com/example/sneaker_store/model/AddressEntity.java
+++ src/main/java/com/example/sneaker_store/model/AddressEntity.java
@@ -1,6 +1,15 @@
 package com.example.sneaker_store.model;
 
+import java.time.Instant;
+
+import com.example.sneaker_store.service.impl.AuthServiceImpl;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,5 +19,31 @@ import lombok.Setter;
 @Entity
 @Table(name = "tbl_address")
 public class AddressEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ward;
+    private String addressLine;
+    private String city;
+    private boolean isDefault;
+
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @PrePersist
+    public void create(){
+        this.createdAt = Instant.now();
+        this.createdBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
+
+    @PreUpdate
+    public void update(){
+        this.updatedAt = Instant.now();
+        this.updatedBy = AuthServiceImpl.getCurrentUserLogin().isPresent() ?
+                AuthServiceImpl.getCurrentUserLogin().get() : null;
+    }
 }
